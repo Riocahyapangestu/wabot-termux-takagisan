a82e0e252d7a96b7b70262a15596364502012f64
Rio Cahya Pangestu <71375367+Riocahyapangestu@users.noreply.github.com>
2021-01-25 23:31:04 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'Bot_Chitanda Eru',
+        botName: 'takagisan',
         ownerName: 'Chitanda Eru',
         youtube: 'https://www.youtube.com/channel/UCMICXHm2oq1h76wM_46O4Cg',
-        instagram: 'https://www.instagram.com/doujinmoee18/',
+        instagram: 'https://www.instagram.com/animelovers_jkt_48',
 }
